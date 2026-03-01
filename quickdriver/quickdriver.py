e6c48276194cd737a75e825dec0b93bf206af925
nishizawatakamasa <nszw0502@gmail.com>
2026-03-02 00:09:57 +0900

    version = "3.0.1"

--- quickdriver/quickdriver.py
+++ quickdriver/quickdriver.py
@@ -7,7 +7,6 @@ from typing import Literal
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
-from selenium.common.exceptions import InvalidArgumentException, TimeoutException
 
 class QuickDriver:
     '''Wrapper for Selenium WebDriver.'''
